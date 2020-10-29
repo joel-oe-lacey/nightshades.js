32bf626136b65bc1d4c70fc21f7d6a093fa91489
Joel Lacey <joellaceybusiness@gmail.com>
2020-10-29 16:44:51 -0400

    Initial hardcode for auth server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,16 @@
-require('dotenv').config()
+// require('dotenv').config()
 const fetch = require('node-fetch');
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
 const params = {
-//   origin: 'YOUR-WEBSITE-URL',
-//   ip: 'THE-WEBSITE-USER-IP',
-//   token: 'YOUR_TREFLE_TOKEN'
+  origin: 'http://localhost:3000',
+//   ip: '68.197.154.229',
+  token: 'kCpt8a54u3_VH15-OQPjwAirxVx0L3TEfy2BYm7eXCY'
 }
 
-(async () => {
+// console.log('envTest', process.env)
+const fetchJWT = async () => {
   const response = await fetch(
     'https://trefle.io/api/auth/claim', {
       method: 'post',
@@ -18,4 +19,6 @@ const params = {
     });
   const json = await response.json();
   console.log(json);
-})();
+}
+
+fetchJWT();
