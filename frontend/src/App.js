c19ca441e029b7619701c6464f09d5c8004e11f1
Joel Lacey <joellaceybusiness@gmail.com>
2020-10-29 16:36:47 -0400

    Add circular loader while fetching combo options

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Box from '@material-ui/core/Box';
 import ProTip from './ProTip';
 import PlantSelector from './PlantSelector';
@@ -10,21 +11,23 @@ import { getPlantIDTuples } from './utils/plantIDs';
 
 const App = () => {
   const [plantData, setPlantData] = useState([])
-
+  // const [selectedPlant, choosePlant] = useState(false)
+  
   useEffect(() => {
     retrieveInitialCollection()
   }, [])
 
   const retrieveInitialCollection = async () => {
-      const token = '&token=eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo4OTk0LCJvcmlnaW4iOiJodHRwOi8vbG9jYWxob3N0OjMwMDAiLCJpcCI6bnVsbCwiZXhwaXJlIjoiMjAyMC0xMC0zMCAxOToxMzoyMiArMDAwMCIsImV4cCI6MTYwNDA4NTIwMn0.cuwIv8OqCEHVnE12ig17BPWmt1YAtDcoDX9dKL0t0XA'
-      const plantList = await apiCall(`https://trefle.io/api/v1/plants?filter_not%5Bedible_part%5D=null` + token)
-      const rawPlantData = await plantList.json();
-      const plantIDData = getPlantIDTuples(rawPlantData.data);
-      if (rawPlantData.links.next) {
-        retrieveSubseqCollections(plantIDData, rawPlantData.links.next, token)
-      } else {
-        setPlantData(plantIDData)
-      }
+    const token = '&token=eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo4OTk0LCJvcmlnaW4iOiJodHRwOi8vbG9jYWxob3N0OjMwMDAiLCJpcCI6bnVsbCwiZXhwaXJlIjoiMjAyMC0xMC0zMCAxOToxMzoyMiArMDAwMCIsImV4cCI6MTYwNDA4NTIwMn0.cuwIv8OqCEHVnE12ig17BPWmt1YAtDcoDX9dKL0t0XA'
+    const plantList = await apiCall(`https://trefle.io/api/v1/plants?filter_not%5Bedible_part%5D=null` + token)
+    const rawPlantData = await plantList.json();
+    const plantIDData = getPlantIDTuples(rawPlantData.data);
+    
+    if (rawPlantData.links.next) {
+      retrieveSubseqCollections(plantIDData, rawPlantData.links.next, token)
+    } else {
+      setPlantData(plantIDData)
+    }
   }
 
   const retrieveSubseqCollections = async (dataSoFar, link, token) => {
@@ -32,15 +35,13 @@ const App = () => {
     const rawPlantData = await plantList.json();
     const plantIDData = getPlantIDTuples(rawPlantData.data);
     const totalPlantData = [...plantIDData, ...dataSoFar]
-    // eslint-disable-next-line no-console
-    console.log(rawPlantData.links.next)
 
-      if (rawPlantData.links.next) {
-        retrieveSubseqCollections(totalPlantData, rawPlantData.links.next, token)
-      } else {
-        setPlantData(totalPlantData)
-      }
+    if (rawPlantData.links.next) {
+      retrieveSubseqCollections(totalPlantData, rawPlantData.links.next, token)
+    } else {
+      setPlantData(totalPlantData)
     }
+  }
 
   return (
     <Container maxWidth="sm">
@@ -48,7 +49,11 @@ const App = () => {
         <Typography variant="h4" component="h1" gutterBottom>
           Preact v4-beta example
         </Typography>
-        <PlantSelector plantData={plantData} />
+        {
+          plantData.length ? 
+          <PlantSelector plantData={plantData} /> 
+          : <CircularProgress />
+        }
         <ProTip />
       </Box>
     </Container>
