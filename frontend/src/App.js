d5eee15e1cabf03cef0102f4bf64e2436e9fe615
Joel Lacey <joellaceybusiness@gmail.com>
2020-11-03 15:05:55 -0500

    Update landing to utilize local JWT auth server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-require('dotenv').config()
 // eslint-disable-next-line no-unused-vars
 import React, { useEffect, useState } from 'react';
 import Container from '@material-ui/core/Container';
@@ -12,27 +11,30 @@ import { getPlantIDTuples } from './utils/plantIDs';
 
 const App = () => {
   const [plantData, setPlantData] = useState([])
-  const [selectedPlant, choosePlant] = useState(false)
+  // const [selectedPlant, choosePlant] = useState(false)
 
   useEffect(() => {
     retrieveInitialCollection()
   }, [])
 
   const retrieveInitialCollection = async () => {
-    const token = process.env.TOKEN;
-    const plantList = await apiCall(`https://trefle.io/api/v1/plants?filter_not%5Bedible_part%5D=null` + token)
+    const tokenFetch = await apiCall(`http://localhost:8080/`);
+    const tokenData = await tokenFetch.json();
+    // eslint-disable-next-line no-console
+    console.log('token', tokenData)
+    const plantList = await apiCall(`https://trefle.io/api/v1/plants?filter_not%5Bedible_part%5D=null&token=${tokenData.token}`);
     const rawPlantData = await plantList.json();
     const plantIDData = getPlantIDTuples(rawPlantData.data);
     
     if (rawPlantData.links.next) {
-      retrieveSubseqCollections(plantIDData, rawPlantData.links.next, token)
+      retrieveSubseqCollections(plantIDData, rawPlantData.links.next, tokenData.token)
     } else {
       setPlantData(plantIDData)
     }
   }
 
   const retrieveSubseqCollections = async (dataSoFar, link, token) => {
-    const plantList = await apiCall('https://trefle.io' + link + token)
+    const plantList = await apiCall(`https://trefle.io${link}&token=${token}`)
     const rawPlantData = await plantList.json();
     const plantIDData = getPlantIDTuples(rawPlantData.data);
     const totalPlantData = [...plantIDData, ...dataSoFar]
@@ -52,7 +54,8 @@ const App = () => {
         </Typography>
         {
           plantData.length ? 
-          <PlantSelector plantData={plantData} choosePlant={choosePlant} /> 
+          <PlantSelector plantData={plantData} /> 
+          // <PlantSelector plantData={plantData} choosePlant={choosePlant} /> 
           : <CircularProgress />
         }
         <ProTip />
