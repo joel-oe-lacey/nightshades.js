43b409ab8cf9e452db2a1040dc1d30ff4eb73a27
Joel Lacey <joellaceybusiness@gmail.com>
2020-11-02 20:43:45 -0500

    Create PlantDetail component

---
+++ frontend/src/PlantDetails.js
@@ -0,0 +1,25 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useEffect, useState } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Container from '@material-ui/core/Container';
+
+const PlantDetails = ({plantData, choosePlant}) => {
+    const retrievePlantData = async () => {
+        const token = process.env.TOKEN;
+        const plantList = await apiCall(`https://trefle.io/api/v1/plants?filter_not%5Bedible_part%5D=null` + token)
+        const rawPlantData = await plantList.json();
+        const plantIDData = getPlantIDTuples(rawPlantData.data);
+        
+        if (rawPlantData.links.next) {
+        retrieveSubseqCollections(plantIDData, rawPlantData.links.next, token)
+        } else {
+        setPlantData(plantIDData)
+        }
+    }
+
+    return (
+
+    );
+}
+
+export default PlantDetails;
