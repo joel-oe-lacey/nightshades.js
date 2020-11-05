21d793989832d8479da40cdc0b9f849272bf5119
Joel Lacey <joellaceybusiness@gmail.com>
2020-11-04 23:30:36 -0500

    Fetch plant on page ID

--- frontend/src/PlantDetails.js
+++ frontend/src/PlantDetails.js
@@ -1,23 +1,25 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useEffect, useState } from 'react';
 import Container from '@material-ui/core/Container';
+import { apiCall } from './utils/fetchCalls';
 
 const PlantDetails = ({id}) => {
+    // eslint-disable-next-line no-console
     // const [plantData, setPlantData] = useState([])
 
-    // useEffect(() => {
-    //     retrievePlantData()
-    // }, [])
+    useEffect(() => {
+        retrievePlantData()
+    }, [])
 
-    // const retrievePlantData = async () => {
-    //     const tokenFetch = await apiCall(`http://localhost:8080/`);
-    //     const tokenData = await tokenFetch.json();
-    //     const plantList = await apiCall(`https://trefle.io/api/v1/plants?filter_not%5Bedible_part%5D=null&token=${tokenData.token}`);
-    //     const rawPlantData = await plantList.json();
-    //     const plantIDData = getPlantIDTuples(rawPlantData.data);
+    const retrievePlantData = async () => {
+        const tokenFetch = await apiCall(`http://localhost:8080/`);
+        const tokenData = await tokenFetch.json();
+        const plantList = await apiCall(`https://trefle.io/api/v1/plants/${id}&token=${tokenData.token}`);
+        const rawPlantData = await plantList.json();
         
-    //     setPlantData(plantIDData)
-    // }
+        // eslint-disable-next-line no-console
+        console.log(rawPlantData)
+    }
 
     return (
         <Container maxWidth="sm">
