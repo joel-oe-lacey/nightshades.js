18c8c433eacaba6dbebe4f1c078e06dc69bfa99d
Joel Lacey <joellaceybusiness@gmail.com>
2020-11-17 21:21:46 -0500

    Add optional chaining support for babel

--- frontend/config/webpack.config.js
+++ frontend/config/webpack.config.js
@@ -342,6 +342,7 @@ module.exports = function(webpackEnv) {
                       },
                     },
                   ],
+                  ['@babel/plugin-proposal-optional-chaining']
                 ],
                 // This is a feature of `babel-loader` for webpack (not Babel itself).
                 // It enables caching results in ./node_modules/.cache/babel-loader/
