df04d6b9306acdf621f9c002b26d6b0ada59f874
Joel Lacey <joellaceybusiness@gmail.com>
2020-12-17 20:13:31 -0500

    Incorporate Images and Info into PlantDetails tab display

--- frontend/src/PlantDetails.js
+++ frontend/src/PlantDetails.js
@@ -1,33 +1,26 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
 import { apiCall } from './utils/fetchCalls';
-import Container from '@material-ui/core/Container';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import EcoIcon from '@material-ui/icons/Eco';
 import Modal from '@material-ui/core/Modal';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import PlantImages from './PlantImages';
+import PlantInfo from './PlantInfo';
 
 const useStyles = makeStyles((theme) => ({
   wrapper: {
-    height: 'max-content',
-    width: '100%',
+    // height: 'max-content',
+    // width: '80%',
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'space-around',
-    alignItems: 'center'
+    alignItems: 'center',
+    outline: 0
   },
   card: {
     height: '80%',
@@ -36,11 +29,7 @@ const useStyles = makeStyles((theme) => ({
   },
   details: {
     height: '25rem',
-    width: '100%',
-    display: 'flex',
-    flexFlow: 'column wrap',
-    justifyContent: 'space-around',
-    alignItems: 'center'
+    width: '100%'
   },
   plantImg: {
     objectFit: 'contain',
@@ -59,13 +48,40 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+function a11yProps(index) {
+  return {
+    id: `plant-details-tab-${index}`,
+    'aria-controls': `plant-details-tabpanel-${index}`,
+  };
+}
+
+function TabPanel(props) {
+  const { children, tab, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={tab !== index}
+      id={`plant-details-tabpanel-${index}`}
+      aria-labelledby={`plant-details-tab-${index}`}
+      {...other}
+    >
+      {tab === index && (
+        <React.Fragment>
+          {children}
+        </React.Fragment>
+      )}
+    </div>
+  );
+}
+
 const PlantDetails = ({open, setOpen, id}) => {
     const classes = useStyles();
     const [plantData, setPlantData] = useState({})
-    const [expanded, setExpanded] = useState(false);
+    const [tab, setTab] = React.useState(0);
 
-    const handleExpandClick = () => {
-        setExpanded(!expanded);
+    const selectTab = (event, newTab) => {
+      setTab(newTab);
     };
 
     const setClosed = () => {
@@ -88,111 +104,49 @@ const PlantDetails = ({open, setOpen, id}) => {
         setPlantData(fetchedPlantData.data);
     }
 
-    // eslint-disable-next-line no-console
-    console.log(plantData)
-
     return (
-        <Container className={classes.wrapper}>
+        <Modal
+            className={classes.wrapper}
+            open={open}
+            onClose={setClosed}
+            aria-labelledby={Object.keys(plantData).length ? plantData.main_species?.common_name : "Plant Details Loading."}
+            aria-describedby="A page of details about this plant."
+        >
             {
                 Object.keys(plantData).length ? 
-                <Modal
-                    open={open}
-                    onClose={setClosed}
-                    aria-labelledby={plantData.main_species.common_name}
-                    aria-describedby="A page of details about this plant."
-                >
                     <Card className={classes.card}>
                         <CardHeader
-                            title={plantData.main_species.common_name}
+                            title={plantData.main_species?.common_name}
                             subheader={plantData.scientific_name}
                         />
                         <CardContent
                             className={classes.details}
                         >
-                            <img 
-                                className={classes.plantImg}
-                                src={plantData.image_url}
-                                alt={plantData.common_name}
+                          <Tabs 
+                            value={tab} 
+                            onChange={selectTab} 
+                            aria-label="Plant Details Selector"
+                            centered
+                          >
+                            <Tab label="Images" {...a11yProps(0)} />
+                            <Tab label="Info" {...a11yProps(1)} />      
+                          </Tabs>
+                          <TabPanel tab={tab} index={0}>
+                            <PlantImages 
+                              plantImages={plantData.main_species?.images}
+                              plantName={plantData.main_species.common_name}
                             />
-                            <Typography variant="header2" color="textSecondary" component="p">
-                                Can Tom Brady eat this?
-                            </Typography>
-                            <Typography variant="header1" color="textSecondary" component="p">
-                                {plantData.family.id === 28 ? 'NO' : 'YES'}
-                            </Typography>
+                          </TabPanel>
+                          <TabPanel tab={tab} index={1}>
+                            <PlantInfo 
+                              plantData={plantData}
+                            />
+                          </TabPanel>
                         </CardContent>
-                        <CardActions disableSpacing>
-                            <IconButton
-                            className={clsx(classes.expand, {
-                                [classes.expandOpen]: expanded,
-                            })}
-                            onClick={handleExpandClick}
-                            aria-expanded={expanded}
-                            aria-label="show details"
-                            >
-                            <ExpandMoreIcon />
-                            </IconButton>
-                        </CardActions>
-                        <Collapse in={expanded} timeout="auto" unmountOnExit>
-                            <CardContent>
-                            <Typography paragraph>
-                                <strong>Scientfic Name:</strong> {plantData.scientific_name}
-                            </Typography>
-                            <Typography paragraph>
-                                <strong>Family:</strong> {plantData.family_common_name} ({plantData.family.name})
-                            </Typography>
-                            <Typography paragraph>
-                                <strong>Genus:</strong> {plantData.genus.name} 
-                            </Typography>
-                            {
-                                plantData?.main_species?.common_names?.en && (
-                                    <React.Fragment>
-                                        <Typography><strong>Other Names:</strong></Typography>
-                                        <List>
-                                            {plantData.main_species.common_names.en.map(name => {
-                                                return (
-                                                    <ListItem>
-                                                        <ListItemIcon>
-                                                            <EcoIcon />
-                                                        </ListItemIcon>
-                                                        <ListItemText
-                                                            primary={name}
-                                                        />
-                                                    </ListItem>
-                                                )
-                                            })}
-                                        </List>
-                                    </React.Fragment>
-                                )
-                            }
-                            {
-                                plantData?.main_species?.distribution?.native && (
-                                    <React.Fragment>
-                                        <Typography><strong>Native Distribution:</strong></Typography>
-                                        <List>
-                                            {plantData.main_species.distribution.native.map(name => {
-                                                return (
-                                                    <ListItem>
-                                                        <ListItemIcon>
-                                                            <EcoIcon />
-                                                        </ListItemIcon>
-                                                        <ListItemText
-                                                            primary={name}
-                                                        />
-                                                    </ListItem>
-                                                )
-                                            })}
-                                        </List>
-                                    </React.Fragment>
-                                )
-                            }
-                            </CardContent>
-                        </Collapse>
                     </Card> 
-                </Modal>
                 : <CircularProgress />
             }
-        </Container>
+        </Modal>
     );
 }
 
