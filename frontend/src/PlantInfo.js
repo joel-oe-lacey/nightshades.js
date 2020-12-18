420e73c82106fa29b5e5651a45796e67cdd06ca4
Joel Lacey <joellaceybusiness@gmail.com>
2020-12-17 20:10:50 -0500

    Build out PlantInfo display

---
+++ frontend/src/PlantInfo.js
@@ -0,0 +1,44 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles(() => ({
+}));
+
+const PlantInfo = ({ plantData }) => {
+    const classes = useStyles();
+
+    return (
+        <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Field</TableCell>
+                        <TableCell align="right">Value</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                {rows.map((row) => (
+                    <TableRow key={row.field}>
+                        <TableCell component="th" scope="row">
+                            {row.field}
+                        </TableCell>
+                        <TableCell align="right">
+                            {row.value}
+                        </TableCell>
+                    </TableRow>
+                ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
+
+export default PlantInfo;
