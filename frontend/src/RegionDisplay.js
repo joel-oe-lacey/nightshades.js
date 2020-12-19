ea47b47b12268e744a5072346f7c9d87d2610b9b
Joel Lacey <joellaceybusiness@gmail.com>
2020-12-18 22:47:51 -0500

    Add in Pagination component to RegionDisplay

--- frontend/src/RegionDisplay.js
+++ frontend/src/RegionDisplay.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useEffect, useState } from 'react';
 import { apiCall } from './utils/fetchCalls';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import PlantCard from './PlantCard';
@@ -10,6 +10,7 @@ import { tdwgRegionsLookup } from './utils/tdwg_regions';
 import PlantDetails from './PlantDetails';
 import IconButton from '@material-ui/core/IconButton';
 import HomeIcon from '@material-ui/icons/Home';
+import Pagination from '@material-ui/lab/Pagination';
 import { route } from 'preact-router';
 
 const useStyles = makeStyles(() => ({
@@ -26,30 +27,47 @@ const useStyles = makeStyles(() => ({
     },
     home: {
         color: 'lightgrey'
-    },
+    }
 }));
+
+const fetchPageCount = lastPath => {
+    // uses regex to find total page count via last link
+    // example url structure
+    // /api/v1/plants?filter%5Bestablishment%5D=native&filter_not%5Bimage_url%5D=null&page=96&zone_id=ari
+    
+    const pageRegex = /page=(\d*)/;
+    const pageCount = pageRegex.exec(lastPath)[1];
+    return Number(pageCount);
+}
+
 
 const RegionDisplay = ({id}) => {
     const classes = useStyles();
     const [plantData, setPlantData] = useState({})
     const [open, setOpen] = useState(false);
     const [chosenPlant, choosePlant] = useState(1);
-    // const [pageNum, setPageNum] = useState(1);
-
-
+    const [page, setPageNum] = useState(1);
+    
     useEffect(() => {
-        retrieveRegionPlantData()
+        retrieveRegionPlantData(page)
     }, [])
 
-    const retrieveRegionPlantData = async () => {
-        const tokenFetch = await apiCall(`http://localhost:8080/`);
-        const tokenData = await tokenFetch.json();
-        const plantList = await apiCall(`https://trefle.io/api/v1/distributions/${id}/plants?filter%5Bestablishment%5D=native&filter_not[image_url]=null&token=${tokenData.token}`);
-        const fetchedPlantData = await plantList.json();
-        
-        setPlantData(fetchedPlantData.data);
+    const changePage = (event, value) => {
+        setPageNum(value)
     }
 
+    const retrieveRegionPlantData = async (page) => {
+        const tokenFetch = await apiCall(`http://localhost:8080/`);
+        const tokenData = await tokenFetch.json();
+        const plantList = await apiCall(`https://trefle.io/api/v1/distributions/${id}/plants?filter%5Bestablishment%5D=native&filter_not[image_url]=null&page=${page}&token=${tokenData.token}`);
+        const fetchedPlantData = await plantList.json();
+        
+        setPlantData(fetchedPlantData);
+    }
+
+    // eslint-disable-next-line no-console
+    console.log(plantData)
+    
     return (
         <Grid container spacing={3} className={classes.grid}>
             <Grid item xs={6}>
@@ -74,12 +92,23 @@ const RegionDisplay = ({id}) => {
             {
                 Object.keys(plantData).length ?
                     <React.Fragment>
-                        {plantData.map(plant => {
+                        {plantData?.data.map(plant => {
                             return (
                                 <PlantCard PlantDetails={plant} choosePlant={choosePlant} setOpen={setOpen} />
                             )
                         })}
                         <PlantDetails open={open} setOpen={setOpen} id={chosenPlant} />
+                        { plantData?.links?.last && 
+                            <Pagination 
+                                count={fetchPageCount(plantData?.links?.last)}
+                                page={page}
+                                onChange={changePage}
+                                variant="outlined" 
+                                color="primary"
+                                size="large"
+                                boundaryCount={2}
+                            /> 
+                        }
                     </React.Fragment>
                 : <CircularProgress />
             }
