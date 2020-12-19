269853c5889993bd71117256b71c1fdb6f7f94a6
Joel Lacey <joellaceybusiness@gmail.com>
2020-12-19 15:06:00 -0500

    Remove unnecessary nest on PlantImages

--- frontend/src/PlantImages.js
+++ frontend/src/PlantImages.js
@@ -4,38 +4,29 @@ import { makeStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
-import ListSubheader from '@material-ui/core/ListSubheader';
 
 const useStyles = makeStyles(() => ({
-
 }));
-
-const generateImageSections = (plantImages, plantName) => {
-    return Object.keys(plantImages).map(imageType => {
-        return plantImages[imageType].map(image => {
-            return (
-                <GridListTile key={image.id}>
-                    <img src={image.image_url} alt={`${plantName} - ${imageType}`} />
-                    <GridListTileBar
-                    title={imageType}
-                    subtitle={<span>{image.copyright}</span>}
-                    />
-                 </GridListTile>
-            )
-        })
-    })
-}
 
 const PlantImages = ({ plantImages, plantName }) => {
     const classes = useStyles();
 
     return (
         <GridList cellHeight={180} className={classes.gridList}>
-            <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
-                <ListSubheader component="div">{plantName}</ListSubheader>
-            </GridListTile>
             {
-                generateImageSections(plantImages, plantName)
+                Object.keys(plantImages).map(imageType => {
+                    return plantImages[imageType].map(image => {
+                        return (
+                            <GridListTile key={image.id}>
+                                <img src={image.image_url} alt={`${plantName} - ${imageType}`} />
+                                <GridListTileBar
+                                title={imageType}
+                                subtitle={<span>{image.copyright}</span>}
+                                />
+                            </GridListTile>
+                        )
+                    })
+                })
             }
         </GridList>
     );
