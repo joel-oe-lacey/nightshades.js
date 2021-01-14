36e9d8662cdde87ccf03664c195f4e070977e7ce
Joel Lacey <joellaceybusiness@gmail.com>
2021-01-13 21:18:33 -0500

    Add cache configuration

--- server/server.js
+++ server/server.js
@@ -10,6 +10,27 @@ const params = {
   token: process.env.TOKEN
 };
 
+// Add Cache Config
+let memCache = new cache.Cache();
+let cacheMiddleware = (duration) => {
+    return (req, res, next) => {
+        let key =  '__express__' + req.originalUrl || req.url
+        let cacheContent = memCache.get(key);
+        if(cacheContent){
+            res.send( cacheContent );
+            return
+        }else{
+            res.sendResponse = res.send
+            res.send = (body) => {
+                memCache.put(key,body,duration*1000);
+                res.sendResponse(body)
+            }
+            next()
+        }
+    }
+};
+
+
 const fetchJWT = async () => {
   const response = await fetch(
     'https://trefle.io/api/auth/claim', {
